e60125764f3d559247ff5824c05aeb826ee3cde4
Farah Juma <fjuma@redhat.com>
2019-07-22 11:14:10 -0400

    wip

--- controller/src/main/java/org/jboss/as/controller/security/CredentialReferenceWriteAttributeHandler.java
+++ controller/src/main/java/org/jboss/as/controller/security/CredentialReferenceWriteAttributeHandler.java
@@ -56,112 +56,20 @@ public class CredentialReferenceWriteAttributeHandler extends ReloadRequiredWrit
         final String alias = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, ALIAS);
         final String secret = CredentialReference.credentialReferencePartAsStringIfDefined(operation.get(VALUE), CLEAR_TEXT);
 
-        final String parentName = PathAddress.pathAddress(operation.get(ModelDescriptionConstants.ADDRESS)).getLastElement().getValue();
-        final String credentialStoreName = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, CredentialReference.STORE);
-        CredentialStoreUpdateService service = (CredentialStoreUpdateService) context.getServiceRegistry(true).getRequiredService(CredentialStoreUpdateService.createServiceName(parentName, credentialStoreName)).getValue();
-        try {
-            service.updateCredentialStore(alias, secret);
-        } catch (CredentialStoreException e) {
-            throw new OperationFailedException(e);
+        if (alias != null && secret != null) {
+            final String parentName = PathAddress.pathAddress(operation.get(ModelDescriptionConstants.ADDRESS)).getLastElement().getValue();
+            final String credentialStoreName = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, CredentialReference.STORE);
+            CredentialStoreUpdateService service = (CredentialStoreUpdateService) context.getServiceRegistry(true).getRequiredService(CredentialStoreUpdateService.createServiceName(parentName, credentialStoreName)).getValue();
+            try {
+                service.updateCredentialStore(alias, secret);
+            } catch (CredentialStoreException e) {
+                throw new OperationFailedException(e);
+            }
         }
         return ! operation.get(VALUE).equals(currentValue);
     }
 
     /*@Override
-    protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName,
-                                           ModelNode resolvedValue, ModelNode currentValue,
-                                           HandbackHolder<Void> handbackHolder) throws OperationFailedException {
-        //final ModelNode model = resource.getModel();
-        //final ModelNode value = model.get(CREDENTIAL_REFERENCE);
-        final String credentialStoreName;
-        final String finalCredentialAlias;
-        final String credentialType;
-        final String secret;
-        String credentialAlias;
-
-        if (resolvedValue.isDefined()) {
-            credentialStoreName = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, CredentialReference.STORE);
-            credentialAlias = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, ALIAS);
-            credentialType = CredentialReference.credentialReferencePartAsStringIfDefined(resolvedValue, CredentialReference.TYPE);
-            secret = CredentialReference.credentialReferencePartAsStringIfDefined(operation.get(VALUE), CLEAR_TEXT);
-        } else {
-            credentialStoreName = null;
-            credentialAlias = null;
-            credentialType = null;
-            secret = null;
-        }
-
-        boolean updateAlias = false;
-        if (credentialStoreName != null && credentialAlias == null && secret != null && ! secret.startsWith("MASK-")) {
-            credentialAlias = "myrandomalias";
-            updateAlias = true;
-        }
-        finalCredentialAlias = credentialAlias;
-
-        final ServiceRegistry serviceRegistry;
-        final ServiceName credentialStoreServiceName;
-        if (credentialStoreName != null) {
-            // use credential store service
-            String credentialStoreCapabilityName = RuntimeCapability.buildDynamicCapabilityName(CREDENTIAL_STORE_CAPABILITY, credentialStoreName);
-            credentialStoreServiceName = context.getCapabilityServiceName(credentialStoreCapabilityName, CredentialStore.class);
-            serviceRegistry = context.getServiceRegistry(false);
-
-            if (finalCredentialAlias != null && secret != null) {
-                CredentialStore credentialStore = CredentialReference.getCredentialStore(serviceRegistry, credentialStoreServiceName);
-                try {
-                    CredentialReference.storeSecret(credentialStore, finalCredentialAlias, secret);
-                    // add generated alias to model
-                    if (updateAlias) {
-                        resolvedValue.get(ALIAS).set(finalCredentialAlias);
-                    }
-
-                    // remove clear-text password from the model
-                    //resolvedValue.get(CLEAR_TEXT).set(new ModelNode());
-                    //currentValue.get(CLEAR_TEXT).set(new ModelNode());
-                    //context.readResource(PathAddress.EMPTY_ADDRESS).getModel().get(CREDENTIAL_REFERENCE).get(CLEAR_TEXT).set(new ModelNode());
-                    *//*context.addStep((context1, operation1) -> {
-                        operation.get(ModelDescriptionConstants.VALUE).get(CLEAR_TEXT).set(new ModelNode());
-                    }, OperationContext.Stage.RUNTIME);*//*
-                } catch (CredentialStoreException e) {
-                    throw new OperationFailedException(e);
-                }
-                *//*if (updateAlias) {
-                    model.get(CREDENTIAL_REFERENCE).get(ALIAS).set(finalCredentialAlias);
-                }
-                model.get(CREDENTIAL_REFERENCE).get(CLEAR_TEXT).set(new ModelNode());
-                /*CredentialStore credentialStore = getCredentialStore(serviceRegistry, credentialStoreServiceName);
-                try {
-                    storeSecret(credentialStore, finalCredentialAlias, secret);
-                    // add generated alias to model
-                    if (updateAlias) {
-                        model.get(CREDENTIAL_REFERENCE).get(ALIAS).set(finalCredentialAlias);
-                    }
-
-                    // remove clear-text password from the model
-                    model.get(CREDENTIAL_REFERENCE).get(CLEAR_TEXT).set(new ModelNode());
-                } catch (CredentialStoreException e) {
-                    throw new OperationFailedException(e);
-                }*//*
-                *//*if (updateAlias) {
-                    model.get(CREDENTIAL_REFERENCE).get(ALIAS).set(finalCredentialAlias);
-                }
-                model.get(CREDENTIAL_REFERENCE).get(CLEAR_TEXT).set(new ModelNode());*//*
-                //ModelNode writeAttributeOperation = Util.getWriteAttributeOperation(context.getCurrentAddress(), CredentialReference.CREDENTIAL_REFERENCE + "." + CredentialReference.ALIAS, finalCredentialAlias);
-                //final OperationStepHandler writeHandler = context.getRootResourceRegistration().getSubModel(context.getCurrentAddress()).getOperationHandler(PathAddress.EMPTY_ADDRESS, WRITE_ATTRIBUTE_OPERATION);
-                //context.addStep(writeAttributeOperation, writeHandler, OperationContext.Stage.RUNTIME);
-
-                //OperationEntry entry = context.getRootResourceRegistration().getOperationEntry(context.getCurrentAddress(), WRITE_ATTRIBUTE_OPERATION);
-                //context.addModelStep(entry.getOperationDefinition(), entry.getOperationHandler(), true);
-
-            }
-        } else {
-            credentialStoreServiceName = null;
-            serviceRegistry = null;
-        }
-        return false;
-    }*/
-
-    @Override
     protected boolean requiresRuntime(OperationContext context) {
         return !context.isBooting();
     }
@@ -171,5 +79,5 @@ public class CredentialReferenceWriteAttributeHandler extends ReloadRequiredWrit
                                          ModelNode valueToRestore, ModelNode valueToRevert, Void handback)
             throws OperationFailedException {
         System.out.println("sf");
-    }
+    }*/
 }
