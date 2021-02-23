654bc906f63009dcbc0db6d270efb381fd9ef7ee
Farah Juma <fjuma@redhat.com>
2021-02-23 18:24:44 -0500

    wip

--- testsuite/elytron/src/test/java/org/wildfly/test/integration/elytron/ssl/OpenSslTlsTestCase.java
+++ testsuite/elytron/src/test/java/org/wildfly/test/integration/elytron/ssl/OpenSslTlsTestCase.java
@@ -15,7 +15,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-
 package org.wildfly.test.integration.elytron.ssl;
 
 import static org.jboss.as.controller.client.helpers.ClientConstants.CONTENT;
@@ -24,49 +23,31 @@ import static org.jboss.as.controller.client.helpers.Operations.createAddOperati
 import static org.jboss.as.controller.client.helpers.Operations.createAddress;
 import static org.jboss.as.controller.client.helpers.Operations.createRemoveOperation;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FilePermission;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.net.URI;
-import java.security.KeyManagementException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.time.Instant;
-import java.time.ZonedDateTime;
+import java.lang.reflect.ReflectPermission;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-import java.util.TimeZone;
-import java.util.concurrent.CompletableFuture;
-import java.util.regex.Pattern;
 
-import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import javax.security.auth.x500.X500Principal;
 
 import org.jboss.as.controller.client.OperationBuilder;
 import org.jboss.as.controller.client.helpers.ClientConstants;
 import org.jboss.as.controller.client.helpers.Operations;
 import org.jboss.as.controller.client.helpers.standalone.ServerDeploymentHelper;
-import org.jboss.as.domain.management.logging.DomainManagementLogger;
+import org.jboss.as.test.integration.management.util.CLIWrapper;
 import org.jboss.as.test.integration.management.util.ServerReload;
-import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.as.test.shared.PermissionUtils;
 import org.jboss.dmr.ModelNode;
-import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceActivator;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.BeforeClass;
@@ -76,38 +57,26 @@ import org.wildfly.core.testrunner.ManagementClient;
 import org.wildfly.core.testrunner.ServerSetup;
 import org.wildfly.core.testrunner.ServerSetupTask;
 import org.wildfly.core.testrunner.WildflyTestRunner;
-import org.wildfly.extension.elytron.Capabilities;
+import org.wildfly.openssl.OpenSSLProvider;
 import org.wildfly.security.ssl.test.util.CAGenerationTool;
 import org.wildfly.security.ssl.test.util.CAGenerationTool.Identity;
-import org.wildfly.security.x500.cert.SelfSignedX509CertificateAndSigningKey;
-import org.wildfly.security.x500.cert.X509CertificateBuilder;
-import org.xnio.OptionMap;
-import org.xnio.Xnio;
-import org.xnio.XnioWorker;
-import org.xnio.ssl.XnioSsl;
-
-import io.undertow.UndertowOptions;
-import io.undertow.client.ClientCallback;
-import io.undertow.client.ClientConnection;
-import io.undertow.client.ClientExchange;
-import io.undertow.client.ClientRequest;
-import io.undertow.client.UndertowClient;
-import io.undertow.protocols.ssl.UndertowXnioSsl;
-import io.undertow.server.DefaultByteBufferPool;
-import io.undertow.util.Headers;
-import io.undertow.util.Methods;
-import io.undertow.util.Protocols;
-import io.undertow.util.StringReadChannelListener;
+import org.wildfly.test.undertow.UndertowSSLService;
+import org.wildfly.test.undertow.UndertowSSLServiceActivator;
+import org.wildfly.test.undertow.UndertowServiceActivator;
 
 @RunWith(WildflyTestRunner.class)
 @ServerSetup(OpenSslTlsTestCase.Setup.class)
 public class OpenSslTlsTestCase {
+
+    CLIWrapper cli;
+    private static final OpenSSLProvider openSslProvider = new OpenSSLProvider();
 
     private static final String javaSpecVersion = System.getProperty("java.specification.version");
     private static final String PASSWORD = "password";
 
     private static final String[] SERVER_KEY_STORE = {"subsystem", "elytron", "key-store", "scarabKS"};
     private static final String[] SERVER_KEY_MANAGER = {"subsystem", "elytron", "key-manager", "ServerKeyManager"};
+    private static final String[] SERVER_SSL_CONTEXT = {"subsystem", "elytron", "server-ssl-context", "test-context"};
     private static final String[] SERVER_SSL_CONTEXT_ALL_PROTOCOLS = {"subsystem", "elytron", "server-ssl-context", "AllProtocolsSSC"};
     private static final String[] SERVER_SSL_CONTEXT_TLS12_ONLY = {"subsystem", "elytron", "server-ssl-context", "TLS12OnlySSC"};
     private static final String[] SERVER_SSL_CONTEXT_TLS13_ONLY = {"subsystem", "elytron", "server-ssl-context", "TLS13OnlySSC"};
@@ -123,9 +92,9 @@ public class OpenSslTlsTestCase {
     private static final String[] TRUST_MANAGER = {"subsystem", "elytron", "trust-manager", "caTrustManager"};
 
     private static CAGenerationTool caGenerationTool = null;
+    private static boolean ignored;
     private static final String JKS_LOCATION = "./target/test-classes/jks";
-
-    private static final Pattern OPENSSL_TLSv13_PATTERN = Pattern.compile("^(TLS_AES_128_GCM_SHA256|TLS_AES_256_GCM_SHA384|TLS_CHACHA20_POLY1305_SHA256|TLS_AES_128_CCM_SHA256|TLS_AES_128_CCM_8_SHA256)$");
+    private static final String TEST_JAR = "test.jar";
 
     @BeforeClass
     public static void noJDK14Plus() {
@@ -141,9 +110,19 @@ public class OpenSslTlsTestCase {
         return Integer.parseInt(javaSpecVersion);
     }
 
-    private boolean isOpenSSL111OrHigher() {
-        SSLContext sslContext = getSslContext("ServerSslContextTLS13OpenSsl", false);
-        if (sslContext == null) {
+    private static boolean isOpenSSL111OrHigher() {
+        AccessController.doPrivileged((PrivilegedAction<Integer>) () -> Security.insertProviderAt(openSslProvider, 1));
+        boolean openSslTls13Enabled = true;
+        try {
+            SSLContext.getInstance("openssl.TLSv1.3");
+        } catch (Exception e) {
+            openSslTls13Enabled = false;
+        }
+        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+            Security.removeProvider(openSslProvider.getName());
+            return null;
+        });
+        /*if (sslContext == null) {
             return false; // running on OpenSSL version < 1.1.1, TLS 1.3 cannot be tested
         }
         String[] OPENSSL_AVAILABLE_CIPHERSUITES = sslContext.getSupportedSSLParameters().getCipherSuites();
@@ -152,23 +131,23 @@ public class OpenSslTlsTestCase {
                 return true;
             }
         }
-        return false;
-    }
-
-    private SSLContext getSslContext(String contextName, boolean assertNotNull) {
-        ServiceName serviceName = Capabilities.SSL_CONTEXT_RUNTIME_CAPABILITY.getCapabilityServiceName(contextName);
-        SSLContext sslContext = (SSLContext) services.getContainer().getService(serviceName).getValue();
-        if (assertNotNull) {
-            Assert.assertNotNull(sslContext);
-        }
-        return sslContext;
+        return false;*/
+        return openSslTls13Enabled;
     }
 
     static class Setup implements ServerSetupTask {
 
         @Override
         public void setup(ManagementClient managementClient) throws Exception {
-            if (isJDK14Plus()) return; // TODO: remove this line once WFCORE-4532 is fixed
+            if (isJDK14Plus()) {
+                // TODO: remove this line once WFCORE-4532 is fixed
+                ignored = true;
+                return;
+            }
+            if (! (getJavaSpecVersion() >= 11 && isOpenSSL111OrHigher())) {
+                ignored = true;
+                return;
+            }
 
             caGenerationTool = CAGenerationTool.builder()
                     .setBaseDir(JKS_LOCATION)
@@ -187,20 +166,21 @@ public class OpenSslTlsTestCase {
 
             createTrustManager(managementClient, TRUST_MANAGER, credential);
 
-            createServerSslContext(managementClient, SERVER_SSL_CONTEXT_ALL_PROTOCOLS, "TLSv1.3 TLSv1.2 TLSv1.1",
-                    "TLS_AES_128_CCM_8_SHA256:TLS_AES_256_GCM_SHA384");
-            createServerSslContext(managementClient, SERVER_SSL_CONTEXT_TLS12_ONLY, "TLSv1.2", null);
-            createServerSslContext(managementClient, SERVER_SSL_CONTEXT_TLS13_ONLY, "TLSv1.3",
-                    "TLS_AES_128_CCM_8_SHA256:TLS_AES_256_GCM_SHA384");
-            createServerSslContext(managementClient, SERVER_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES, "TLSv1.3 TLSv1.2 TLSv1.1", null);
+            createServerSslContext(managementClient, SERVER_SSL_CONTEXT, null, null);
+            //createServerSslContext(managementClient, SERVER_SSL_CONTEXT, "TLSv1.3 TLSv1.2 TLSv1.1",
+              //      "TLS_AES_128_CCM_8_SHA256:TLS_AES_256_GCM_SHA384");
+            //createServerSslContext(managementClient, SERVER_SSL_CONTEXT_TLS12_ONLY, "TLSv1.2", null);
+            //createServerSslContext(managementClient, SERVER_SSL_CONTEXT_TLS13_ONLY, "TLSv1.3",
+                //    "TLS_AES_128_CCM_8_SHA256:TLS_AES_256_GCM_SHA384");
+            //createServerSslContext(managementClient, SERVER_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES, "TLSv1.3 TLSv1.2 TLSv1.1", null);
 
-            createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_ALL_PROTOCOLS, "TLSv1.3 TLSv1.2 TLSv1.1",
-                    "TLS_AES_256_GCM_SHA384:TLS_AES_128_CCM_8_SHA256");
-            createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_TLS13_ONLY, "TLSv1.3",
-                    "TLS_AES_128_GCM_SHA256");
-            createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES, "TLSv1.3 TLSv1.2 TLSv1.1", null);
+            //createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_ALL_PROTOCOLS, "TLSv1.3 TLSv1.2 TLSv1.1",
+            //        "TLS_AES_256_GCM_SHA384:TLS_AES_128_CCM_8_SHA256");
+            //createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_TLS13_ONLY, "TLSv1.3",
+            //        "TLS_AES_128_GCM_SHA256");
+            //createClientSslContext(managementClient, CLIENT_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES, "TLSv1.3 TLSv1.2 TLSv1.1", null);
 
-            /*JavaArchive jar = ShrinkWrap.create(JavaArchive.class, TEST_JAR)
+            JavaArchive jar = ShrinkWrap.create(JavaArchive.class, TEST_JAR)
                     .addClasses(UndertowServiceActivator.DEPENDENCIES)
                     .addClasses(UndertowSSLService.class)
                     .addAsResource(new StringAsset("Dependencies: io.undertow.core"), "META-INF/MANIFEST.MF")
@@ -210,32 +190,34 @@ public class OpenSslTlsTestCase {
                             new RuntimePermission("accessClassInPackage.sun.security.ssl"),
                             new ReflectPermission("suppressAccessChecks"))), "permissions.xml")
                     .addAsServiceProviderAndClasses(ServiceActivator.class, UndertowSSLServiceActivator.class);
-            deploy(jar, managementClient);*/
+            deploy(jar, managementClient);
 
             ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
         }
 
         @Override
         public void tearDown(ManagementClient managementClient) throws Exception {
-            managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_TLS13_ONLY)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_ALL_PROTOCOLS)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_TLS13_ONLY)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_TLS12_ONLY)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_ALL_PROTOCOLS)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(TRUST_MANAGER)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_KEY_MANAGER)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_KEY_MANAGER)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(TRUST_STORE)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_KEY_STORE)));
-            managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_KEY_STORE)));
+            if (! ignored) {
+                //managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_TLS13_ONLY)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_SSL_CONTEXT_ALL_PROTOCOLS)));
+                managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_NO_TLS13_CIPHER_SUITES)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_TLS13_ONLY)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_TLS12_ONLY)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_SSL_CONTEXT_ALL_PROTOCOLS)));
+                managementClient.executeForResult(createRemoveOperation(createAddress(TRUST_MANAGER)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_KEY_MANAGER)));
+                managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_KEY_MANAGER)));
+                managementClient.executeForResult(createRemoveOperation(createAddress(TRUST_STORE)));
+                //managementClient.executeForResult(createRemoveOperation(createAddress(CLIENT_KEY_STORE)));
+                managementClient.executeForResult(createRemoveOperation(createAddress(SERVER_KEY_STORE)));
 
 
-           // undeploy(managementClient, TEST_JAR);
-            ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
-            caGenerationTool.close();
-
+                undeploy(managementClient, TEST_JAR);
+                ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
+                caGenerationTool.close();
+            }
         }
 
         private void createKeyStore(ManagementClient managementClient, String[] address, String path, ModelNode credential) throws Exception {
@@ -256,6 +238,7 @@ public class OpenSslTlsTestCase {
 
         private void createKeyManager(ManagementClient managementClient, String[] address, ModelNode credential, boolean isServer) throws Exception {
             ModelNode modelNode = createAddOperation(createAddress(address));
+            modelNode.get("algorithm").set(keyAlgorithm());
             modelNode.get("key-store").set(isServer ? SERVER_KEY_STORE[3] : CLIENT_KEY_STORE[3]);
             modelNode.get("credential-reference").set(credential);
             managementClient.executeForResult(modelNode);
@@ -281,10 +264,14 @@ public class OpenSslTlsTestCase {
             modelNode.get("key-manager").set(isServer ? SERVER_KEY_MANAGER[3] : CLIENT_KEY_MANAGER[3]);
             modelNode.get("trust-manager").set(TRUST_MANAGER[3]);
             if (protocols != null) {
-                modelNode.get("protocols").set(new ModelNode().add(protocols));
+                ModelNode protocolsList = new ModelNode();
+                for (String protocol : protocols.split(" ")) {
+                    protocolsList.add(protocol);
+                }
+                modelNode.get("protocols").set(protocolsList);
             }
             if (cipherSuiteNames != null) {
-                modelNode.get("cipher-suite-names").set(new ModelNode().add(cipherSuiteNames));
+                modelNode.get("cipher-suite-names").set(cipherSuiteNames);
             }
             modelNode.get("providers").set("openssl");
             managementClient.executeForResult(modelNode);
@@ -293,17 +280,20 @@ public class OpenSslTlsTestCase {
 
     @Test
     public void testSslServiceAuthTLS13OpenSsl() throws Throwable {
-        Assume.assumeTrue("Skipping testSslServiceAuthTLS13OpenSsl, test is not being run with JDK 11+ and OpenSSL 1.1.1+",
-                getJavaSpecVersion() >= 11 && isOpenSSL111OrHigher());
+        //Assume.assumeTrue("Skipping testSslServiceAuthTLS13OpenSsl, test is not being run with JDK 11+ and OpenSSL 1.1.1+",
+          //      getJavaSpecVersion() >= 11 && isOpenSSL111OrHigher());
+        Assert.assertEquals("blah", "blah");
         //testCommunication("ServerSslContextTLS13OpenSsl", "ClientSslContextTLS13OpenSsl", false, "OU=Elytron,O=Elytron,C=CZ,ST=Elytron,CN=localhost",
         //        "OU=Elytron,O=Elytron,C=UK,ST=Elytron,CN=Firefly", "TLS_AES_256_GCM_SHA384", true);
     }
+
 
     /**
      * Determines key algorithm to be used based on the used JDK.
      *
      * @return IbmX509 in case of IBM JDK, SunX509 otherwise
      */
+
     private static String keyAlgorithm() {
         if (isIbmJdk()) {
             return "IbmX509";
@@ -325,6 +315,7 @@ public class OpenSslTlsTestCase {
      * @param archive the archive to deploy
      * @throws IOException if an error occurs deploying the archive
      */
+
     public static void deploy(final Archive<?> archive, ManagementClient managementClient) throws IOException {
         // Use an operation to allow overriding the runtime name
         final ModelNode address = Operations.createAddress(DEPLOYMENT, archive.getName());
@@ -366,3 +357,4 @@ public class OpenSslTlsTestCase {
         }
     }
 }
+
